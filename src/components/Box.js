e016444e6a97b78680de59ce647f678fd2282036
Matt Neary <neary@kensho.com>
2017-06-03 07:42:37 -0400

    make it alive

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,26 +6,26 @@ import {
 import {withState} from 'recompose'
 import cx from 'classnames'
 
-const EXPIRY = 1e3
+const EXPIRY = 400
+const WIGGLE = 0.3
+const PERIOD = 60
+const SIZE = 30
 const gc = (rn, xs) => xs.filter(({time}) => rn - time <= EXPIRY)
 
 class Box extends Component {
   componentWillMount() {
-    this.draw()
+    this.draw(0)
   }
 
-  drawPoint = (ctx, t, point) => {
+  drawPoint = (ctx, t, point, scalar = 1) => {
     const rotation = 0
     const [x, y, rX, rY] = point
     ctx.beginPath()
     ctx.save()
     ctx.globalAlpha = Math.pow(1 - t, 2)
-    ctx.ellipse(x, y, rX || 30, rY || 30, rotation, 0, 2 * Math.PI, false)
-    ctx.lineWidth = 3
-    ctx.strokeStyle = 'blue'
+    ctx.ellipse(x, y, scalar * (rX || SIZE), scalar * (rY || SIZE), rotation, 0, 2 * Math.PI, false)
     ctx.fillStyle = 'white'
     ctx.fill()
-    ctx.stroke()
     ctx.restore()
     ctx.closePath()
   }
@@ -43,13 +43,13 @@ class Box extends Component {
     ctx.lineTo(width, y)
     ctx.moveTo(x, 0)
     ctx.lineTo(x, height)
-    ctx.strokeStyle = 'blue'
+    ctx.strokeStyle = 'white'
     ctx.lineWidth = 1
     ctx.stroke()
     ctx.closePath()
 
     ctx.beginPath()
-    ctx.fillStyle = 'blue'
+    ctx.fillStyle = 'white'
     ctx.font = '14px Futura'
     ctx.fillText(yLabel, width - ySize.width - 10, y - 18)
     ctx.closePath()
@@ -63,16 +63,26 @@ class Box extends Component {
     ctx.restore()
   }
 
-  drawRadius = (ctx, point) => {
+  drawFPS = (ctx, fps) => {
+    const label = `${fps.toFixed(0)} FPS`
+    const size = ctx.measureText(label)
+    ctx.beginPath()
+    ctx.fillStyle = 'white'
+    ctx.font = '18px Futura'
+    ctx.fillText(label, 10, 24)
+    ctx.closePath()
+  }
+
+  drawRadius = (ctx, point, scalar = 2) => {
     const rotation = 0
     const [x, y, rX, rY] = point
     const width = innerWidth
     const height = innerHeight
 
     ctx.beginPath()
-    ctx.ellipse(x, y, (rX || 30) * 2, (rY || 30) * 2, rotation, 0, 2 * Math.PI, false)
-    ctx.lineWidth = 1
-    ctx.strokeStyle = 'blue'
+    ctx.ellipse(x, y, (rX || SIZE) * 2, (rY || SIZE) * 2, rotation, 0, 2 * Math.PI, false)
+    ctx.lineWidth = scalar
+    ctx.strokeStyle = 'white'
     ctx.stroke()
     ctx.closePath()
 
@@ -81,22 +91,21 @@ class Box extends Component {
     const xLabel = rX ? rX.toFixed(2) : '?'
     const xSize = ctx.measureText(xLabel)
     ctx.beginPath()
-    ctx.fillStyle = 'blue'
+    ctx.fillStyle = 'white'
     ctx.font = '14px Futura'
-    ctx.fillText(yLabel, x + 4, y - (rY || 30) * 2 - 4)
+    ctx.fillText(yLabel, x + 4, y - (rY || SIZE) * 2 - 4)
     ctx.closePath()
 
     ctx.save()
     ctx.beginPath()
-    ctx.translate(x - (rX || 30) * 2 - 18, y)
+    ctx.translate(x - (rX || SIZE) * 2 - 18, y)
     ctx.rotate(Math.PI / 2)
     ctx.fillText(xLabel, 4, 4)
     ctx.closePath()
     ctx.restore()
   }
 
-  draw = () => {
-    const scale = devicePixelRatio
+  draw = step => {
     const width = innerWidth
     const height = innerHeight
     if (this._ctx) {
@@ -107,38 +116,42 @@ class Box extends Component {
       const ctx = this._ctx
       ctx.clearRect(0, 0, width, height)
 
-      ctx.beginPath()
-      ctx.rect(1, 1, width - 3, height - 3)
-      ctx.strokeStyle = 'blue'
-      ctx.stroke()
-      ctx.closePath()
-
       const rn = now()
-      const trails = flatten(values(this.props.touches).map(initial))
+      const trails = gc(rn, flatten(values(this.props.touches).map(initial)))
       const ghosts = [...this.props.ghosts, ...trails]
 
       gc(rn, ghosts).forEach(({time, point}) =>
          this.drawPoint(ctx, (rn - time) / EXPIRY, point)
       )
 
-      values(this.props.touches).forEach(trail => {
-        if (!trail.length) return
-        const {point} = last(trail)
-        if (trail.length > 1) {
-          const start = trail[0].point
-          const [startX, startY] = start
-          this.drawPoint(ctx, 0, start)
-          const pyth = (a, b) => Math.sqrt(a * a + b * b) / 2
-          const radius = pyth(point[0] - startX, point[1] - startY)
-          this.drawRadius(ctx, [startX, startY, radius, radius])
+      values(this.props.touches).forEach(history => {
+        const {point} = last(history)
+        if (history) {
+          const start = history[0].point
+          const [startX, startY, rX, rY] = start
+          const distance = Math.abs(PERIOD - step)
+          const radius = Math.hypot(point[0] - startX, point[1] - startY) / 2
+          this.drawPoint(ctx, 0, start, 1 + WIGGLE * Math.sqrt(distance / (PERIOD / 2)) * Math.log1p(radius / Math.max(rX || 30, rY)))
+          this.drawRadius(
+            ctx,
+            [startX, startY, radius, radius],
+            step < 6 ? 6 - step + 1 : 2,
+          )
         }
 
         this.drawCrosshair(ctx, point)
         this.drawRadius(ctx, point)
         this.drawPoint(ctx, 0, point)
       })
+
+      if (this.props.prevTime !== null) {
+        const delta = (rn - this.props.prevTime) / 1e3
+        if (!this._fps || step === 0) this._fps = 1 / delta
+        this.drawFPS(ctx, this._fps)
+      }
+      this.props.setPrevTime(rn)
     }
-    requestAnimationFrame(() => this.draw())
+    requestAnimationFrame(() => this.draw((step + 1) % PERIOD))
   }
 
   action = type => evt => {
@@ -198,6 +211,7 @@ class Box extends Component {
 }
 
 export default compose(
+  withState('prevTime', 'setPrevTime', null),
   withState('touches', 'setTouches', {}),
   withState('ghosts', 'setGhosts', []),
 )(Box)
